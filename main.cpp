0af5a2b4df17c7525cb67930b8af76525891cac0
Walt <waltduivenvoorde@gmail.com>
2024-11-15 10:58:35 +0100

    Third commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <unistd.h>
 #include <vector>
+#include <cstring>
 
 int logCommit(std::string msg) {
 	std::ofstream logFile("log.txt", std::ios_base::in);
@@ -24,8 +25,6 @@ int logCommit(std::string msg) {
 int main(int argc, char* argv[]) {
 	std::cout << "Entered custom git wrapper\n";
 
-	std::cout << "AAAAAA\n";
-
   std::vector<char*> execArgs;
   execArgs.push_back((char*)"/usr/bin/git"); 
   for (int i = 1; i < argc; ++i) {
@@ -36,6 +35,12 @@ int main(int argc, char* argv[]) {
   for (int i = 0; i < (int)execArgs.size(); i += 1) {
   	printf("%s ", execArgs[i]);
   } printf("\n");
+
+  if (argc > 2) {
+	  if (strcmp(execArgs[1], "commit") == 0 && strcmp(execArgs[2], "-m") == 0) {
+	  	logCommit(execArgs[3]);
+  	}
+  }
 
   execvp(execArgs[0], execArgs.data());
   perror("Error executing git");
