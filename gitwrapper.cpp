6dbebb8b7362ecb5b00cbd85539943e88c8aa900
Walt <waltduivenvoorde@gmail.com>
2024-11-22 18:23:11 +0100

    Eighteenth commit

--- gitwrapper.cpp
+++ gitwrapper.cpp
@@ -5,16 +5,63 @@
 #include <unistd.h>
 #include <vector>
 #include <cstring>
+#include <regex>
 #include <sys/wait.h>
 #include <filesystem>
+
+std::string timeSpent(
+	const std::time_t nowTimeT, 
+	const std::string logFileName) 
+{
+	std::ifstream logFile(logFileName);
+	if (!logFile.is_open()) {
+		perror("Failed to open log");
+		return "-1";
+	}
+	std::string line;
+	std::string lastLine;
+  while (std::getline(logFile, line)) {
+    if (!line.empty()) {
+      lastLine = line;
+    }
+  }
+  logFile.close();
+
+  std::regex reg(R"(\[(.*)\] - \[(.*)\] '(.*)')");
+  std::smatch match;
+
+  if (std::regex_match(lastLine, match, reg)) {
+  	std::string lastTimeStampStr = match[1].str();
+
+    std::tm lastTimeTm = {};
+    std::istringstream ss(lastTimeStampStr);
+    ss >> std::get_time(&lastTimeTm, "%a %b %d %H:%M:%S %Y");
+    if (ss.fail()) {
+      std::cerr << "Failed to parse timestamp: " << lastTimeStampStr << '\n';
+      return "-1";
+    }
+
+    auto lastTimeT = std::mktime(&lastTimeTm);
+
+    auto diff = std::difftime(nowTimeT, lastTimeT);
+    int hours = static_cast<int>(diff) / 3600;
+    int minutes = (static_cast<int>(diff) % 3600) / 60;
+    int seconds = static_cast<int>(diff) % 60;
+
+    std::ostringstream timeSpentStr;
+    timeSpentStr << hours << "h " << minutes << "m " << seconds << "s";
+    return timeSpentStr.str();
+  }
+
+  std::cerr << "No valid timestamp found in last log line\n";
+  return "-1";
+}
 
 int logCommit(std::string msg) {
 	auto execPath = std::filesystem::canonical("/proc/self/exe").parent_path();
 	std::string logFileName = execPath.string() + "/log.txt";
 
-	std::cout << logFileName << '\n';
-
-	std::ofstream logFile(logFileName, std::ios_base::app);
+	std::fstream logFile(logFileName, std::ios_base::app);
 	if (!logFile.is_open()) {
 		perror("Failed to open log");
 		return 1;
@@ -27,11 +74,11 @@ int logCommit(std::string msg) {
 		nowStr.pop_back();
 	}
 
-	logFile << '[' << nowStr << "] '" << msg << "'\n";
+	logFile << '[' << nowStr << "] - [" << timeSpent(nowT, logFileName) << "] '" << msg << "'\n";
 	logFile.close();
 
 	std::cerr << "Added:\n";
-	std::cerr << '[' << nowStr << "] '" << msg << "'\n";
+	std::cerr << '[' << nowStr << "] - [" << timeSpent(nowT, logFileName) << "] '" << msg << "'\n";
 
 	return 0;
 }
