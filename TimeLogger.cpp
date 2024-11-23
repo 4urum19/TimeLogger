580f81d32bea2ffd082d7d46e5d237fe16d52894
Walt <waltduivenvoorde@gmail.com>
2024-11-23 20:12:56 +0100

    Fourth Commit

--- TimeLogger.cpp
+++ TimeLogger.cpp
@@ -4,6 +4,48 @@
 #include <ctime>
 #include <filesystem>
 #include <iomanip>
+#include <vector>
+
+std::vector<std::string> copySmatch(std::smatch match) {
+	std::vector<std::string> v;
+	for (size_t i = 0; i < match.size(); i += 1) {
+		v.push_back(match[i]);
+	}
+	return v;
+}
+
+std::time_t stringToTimeT(std::string dateStr, std::string format) {
+	std::tm time = {};
+	std::istringstream ssi(dateStr);
+
+	ssi >> std::get_time(&time, format.c_str());
+	if (ssi.fail()) {
+		perror("Failed to convert std::string to tm");
+		return {};
+	}
+
+	return mktime(&time);
+}
+
+std::string getTimeSpent(std::smatch match, std::vector<std::string> prevMatch) {
+	std::string prevDate = prevMatch[1] + " " + prevMatch[2];
+	std::string date = (std::string)match[1] + " " + (std::string)match[2];
+	std::string format = "%d-%m-%Y %H:%M:%S";
+
+	std::time_t prevTime = stringToTimeT(prevDate, format);
+	std::time_t time = stringToTimeT(date, format);
+
+	uint32_t seconds = (uint32_t)difftime(time, prevTime);
+	uint32_t hours = seconds / 3600;
+	uint32_t minutes = (seconds % 3600) / 60;
+	uint32_t sec = seconds % 60;
+
+	std::ostringstream oss;
+	oss << std::setw(2) << std::setfill('0') << hours << ":"
+			<< std::setw(2) << std::setfill('0') << minutes << ":"
+			<< std::setw(2) << std::setfill('0') << sec;
+	return oss.str();
+}
 
 std::string getCurrentDate() {
 	std::time_t now = std::time(NULL);
@@ -36,10 +78,18 @@ int printLog(std::string date) {
 	}
 
 	std::ostringstream oss;
+	std::vector<std::string> prevMatch;
 	oss << match[1] << ":\n";
 	do {
     if (regex_match(line, match, reg) && match[1] == date) {
+    	std::string timeSpent = "";
+    	if (!prevMatch.empty()) {
+    		timeSpent = getTimeSpent(match, prevMatch);
+    	}
+ 			prevMatch = copySmatch(match);
+
  			oss << std::setw(8) << std::setfill(' ') << match[2] << " | " 
+ 					 << timeSpent << " | "
 					<< match[3] << '\n';
     }
 		if (logFile.eof()) break;
