877f06679a33696d69549e5d1464acf0481a998f
Walt <waltduivenvoorde@gmail.com>
2024-11-23 14:00:34 +0100

    Twentysecond commit

--- gitwrapper.cpp
+++ gitwrapper.cpp
@@ -26,7 +26,7 @@ int logCommit(std::string msg) {
 	std::string curDate;
 	std::string curTime;
 
-	std::regex reg(R"(\w+ \w+ \d+) ([0-9:]+))");
+	std::regex reg(R"(([A-Za-z]+ [A-Za-z]+ [0-9]+]) ([0-9:]+))");
 	std::smatch match;
 	if (std::regex_match(nowStr, match, reg)) {
 		std::cout << "enter reg";
