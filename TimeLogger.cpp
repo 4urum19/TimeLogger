984bfceabe4f67d0989928cc664e8c5fc0b870cc
Walt <waltduivenvoorde@gmail.com>
2024-11-24 12:52:50 +0100

    First commit

--- TimeLogger.cpp
+++ TimeLogger.cpp
@@ -5,6 +5,7 @@
 #include <filesystem>
 #include <iomanip>
 #include <vector>
+#include <getopt.h>
 
 std::vector<std::string> copySmatch(std::smatch match) {
 	std::vector<std::string> v;
@@ -59,6 +60,18 @@ std::string getCurrentDate() {
 	return oss.str();
 }
 
+std::string getCurrentTime() {
+	std::time_t now = std::time(NULL);
+	std::tm localTime;
+	localtime_r(&now, &localTime);
+
+	std::ostringstream oss;
+	oss << std::setw(2) << std::setfill('0') << localTime.tm_hour << ':'
+			<< std::setw(2) << std::setfill('0') << localTime.tm_min << ':'
+			<< std::setw(2) << std::setfill('0') << localTime.tm_sec;
+	return oss.str();
+}
+
 int printLog(std::string date) {
 	auto execPath = std::filesystem::canonical("/proc/self/exe").parent_path();
 	std::string logFileName = execPath.string() + "/log.txt";
@@ -104,11 +117,67 @@ int printLog(std::string date) {
 	return 0;
 }
 
+void showHelp(const char* progName) {
+  std::cerr << progName << "{-d [DD-MM-YYYY]} {-b|-e} \n";
+  std::cerr <<
+R"HERE(
+    -h		Print help menu
+    -d		Print log van een gegeven dag (DD-MM-YYYY format)
+    			Default huidige datum
+    -b		Begin dag
+    -e 		Eindig dag
+)HERE";
+}
+
+int addMetaMsg(std::string msg) {
+	auto execPath = std::filesystem::canonical("/proc/self/exe").parent_path();
+	std::string logFileName = execPath.string() + "/log.txt";
+
+	std::fstream logFile(logFileName, std::ios_base::app);
+	if (!logFile.is_open()) {
+		perror("Failed to open log");
+		return 1;
+	}
+
+	std::string curDate = getCurrentDate();
+	std::string curTime = getCurrentTime();
+
+	logFile << '[' << curDate << "] [" << curTime << "] '" << msg << "'\n";
+	logFile.close();
+
+	return 0;
+}
+
 int main(int argc, char* argv[]) {
 	clock_t tStart = clock();
 
-	std::string date = getCurrentDate();
-	std::string testDate = "23-11-2030";
+	const char *progName = argv[0];
+	char c;
+	std::string date;
+	
+	while(c = getopt(argc, argv, "d:beh")) {
+		switch(c) {
+			case 'd':
+				date = optarg;
+				printf("%s\n", date);
+				printLog(date);
+				return 0;
+			case 'b':
+				addMetaMsg("Start dag");
+				return 0;
+			case 'e':
+				addMetaMsg("Eindig dag");
+				return 0;
+			case '?':
+				std::cerr << "Onbekende optie\n\n";
+			case 'h':
+			default:
+				showHelp(progName);
+				return 1;
+		}
+	}
+
+	date = getCurrentDate();
 	printLog(date);
 
 	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
